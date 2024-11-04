ccc5954d0102b67f0d597fce346eee618650e925
Maulik-Manipal <maulik.219310274@muj.manipal.edu>
2024-11-04 15:51:00 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "textSummarizer"
+
+list_of_files = [
+    ".github/workflows/.gitkeep"
+    f"src/{project_name}__init__.py",
+    f"src/{project_name}components/__init__.py",
+    f"src/{project_name}utils/__init__.py",
+    f"src/{project_name}utils/common.py",
+    f"src/{project_name}logging/__init__.py",
+    f"src/{project_name}config/__init__.py",
+    f"src/{project_name}config/configuration.py",
+    f"src/{project_name}pipeline/__init__.py",
+    f"src/{project_name}entity/__init__.py",
+    f"src/{project_name}constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "requirements.text",
+    "setup.py",
+    "research/trials.ipynb",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath) # it will return the directory and filename
+
+    if filedir != "": # to check if the filedir is empty or not
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0): #if my file size is 0 then I will create a file
+        with open(filepath, "w") as file:# w is writing mode
+            file.write("# This is a placeholder file for the project\n")
+            logging.info(f"Created empty file: {filepath}")
+    
+    else:
+        logging.info(f"{filename} is already exist.")
+    
